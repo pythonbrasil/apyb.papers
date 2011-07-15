aa5c24bb280d5c925d750a018ce1c7c53c0392cd
Érico Andrei <erico@simplesconsultoria.com.br>
2011-07-15 02:48:18 -0300

    Improve views

--- apyb/papers/speaker.py
+++ apyb/papers/speaker.py
@@ -2,6 +2,9 @@
 from five import grok
 from plone.directives import dexterity, form
 
+from Acquisition import aq_inner
+from zope.component import getMultiAdapter
+
 from plone.namedfile.field import NamedImage
 
 from zope import schema
@@ -55,7 +58,14 @@ class ISpeaker(form.Schema):
         required=True,
         description=_(u"Speaker's site"),
     )
-    
+    #
+    language = schema.Choice(
+        title=_(u"Language"),
+        required=True,
+        description=_(u"Speaker's language"),
+        vocabulary='apyb.papers.languages',
+    )
+    #
     image = NamedImage(
         title=_(u"Portrait"),
         required=False,
@@ -76,6 +86,9 @@ class Speaker(dexterity.Item):
     def Title(self):
         return self.title
     
+    def Description(self):
+        return self.description
+    
     def UID(self):
         return self.uid
     
@@ -85,8 +98,38 @@ class Speaker(dexterity.Item):
         return intids.getId(self)
     
     
-class SampleView(grok.View):
+class View(grok.View):
     grok.context(ISpeaker)
     grok.require('zope2.View')
     
-    # grok.name('view')
+    def update(self):
+        super(View,self).update()
+        context = aq_inner(self.context)
+        self._path = '/'.join(context.getPhysicalPath())
+        self.state = getMultiAdapter((context, self.request), name=u'plone_context_state')
+        self.tools = getMultiAdapter((context, self.request), name=u'plone_tools')
+        self.portal = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+        self._ct = self.tools.catalog()
+        self._mt = self.tools.membership()
+        self.member = self.portal.member()
+        roles_context = self.member.getRolesInContext(context)
+        if not self.show_border:
+            self.request['disable_border'] = True
+    
+    @property
+    def show_border(self):
+        ''' Is this user allowed to edit this content '''
+        return self.state.is_editable()
+        
+    def speaker_name(self,speaker_uids):
+        ''' Given a list os uids, we return a string with speakers names '''
+        ct = self._ct
+        results = ct.searchResults(portal_type='apyb.papers.speaker',UID=speaker_uids)
+        return ', '.join([b.Title for b in results])
+     
+    def my_talks(self):
+        ''' Return a list of my talks '''
+        results = self._ct.searchResults(portal_type='apyb.papers.talk', 
+                                         Creator=self.context.email,
+                                         sort_on='sortable_title')
+        return results
--- apyb/papers/track.py
+++ apyb/papers/track.py
@@ -1,6 +1,11 @@
 from five import grok
 from plone.directives import dexterity, form
 
+from Acquisition import aq_inner
+from zope.component import getMultiAdapter
+
+from plone.namedfile.field import NamedImage
+
 from zope import schema
 
 from zope.component import getUtility
@@ -34,7 +39,7 @@ class ITrack(form.Schema):
         description=_(u"A brief description of this track."),
     )
     
-    image = schema.Bytes(
+    image = NamedImage(
         title=_(u"Track Logo"),
         required=False,
         description=_(u"Upload an image to be used as this track's logo."),
@@ -45,10 +50,11 @@ class ITrack(form.Schema):
 class Track(dexterity.Container):
     grok.implements(ITrack)
     
-    # Add your class methods and properties here
-
     def Title(self):
         return self.title
+    
+    def Description(self):
+        return self.description
     
     def UID(self):
         return self.uid
@@ -58,8 +64,46 @@ class Track(dexterity.Container):
         intids = getUtility(IIntIds)
         return intids.getId(self)
 
-class SampleView(grok.View):
+class View(grok.View):
     grok.context(ITrack)
     grok.require('zope2.View')
     
-    # grok.name('view')
+    def update(self):
+        super(View,self).update()
+        context = aq_inner(self.context)
+        self._path = '/'.join(context.getPhysicalPath())
+        self.state = getMultiAdapter((context, self.request), name=u'plone_context_state')
+        self.tools = getMultiAdapter((context, self.request), name=u'plone_tools')
+        self.portal = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+        self._ct = self.tools.catalog()
+        self._mt = self.tools.membership()
+        self.member = self.portal.member()
+        roles_context = self.member.getRolesInContext(context)
+        if not self.show_border:
+            self.request['disable_border'] = True
+    
+    
+    def speaker_name(self,speaker_uids):
+        ''' Given a list os uids, we return a string with speakers names '''
+        ct = self._ct
+        results = ct.searchResults(portal_type='apyb.papers.speaker',UID=speaker_uids)
+        return ', '.join([b.Title for b in results])
+    
+    @property
+    def can_submit(self):
+        ''' This user can submit a talk in here'''
+        context = self.context
+        return self._mt.checkPermission('apyb.papers: Add Talk',context)
+    
+    @property
+    def show_border(self):
+        ''' Is this user allowed to edit this content '''
+        return self.state.is_editable()
+    
+    def talks(self):
+        ''' Return a list of talks in here '''
+        results = self._ct.searchResults(portal_type='apyb.papers.talk', 
+                                         path=self._path,
+                                         sort_on='sortable_title')
+        return results
+    
