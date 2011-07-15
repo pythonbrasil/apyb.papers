f2e0b5089972341a0583eda132a0a961d7dc402a
Érico Andrei <erico@simplesconsultoria.com.br>
2011-07-15 02:45:14 -0300

    Improve default views

--- apyb/papers/program.py
+++ apyb/papers/program.py
@@ -1,4 +1,9 @@
+# -*- coding:utf-8 -*-
 from five import grok
+
+from Acquisition import aq_inner
+from zope.component import getMultiAdapter
+
 from plone.directives import dexterity, form
 
 from zope import schema
@@ -24,8 +29,119 @@ class Program(dexterity.Container):
 
 
 
-class SampleView(grok.View):
+class View(grok.View):
     grok.context(IProgram)
     grok.require('zope2.View')
     
-    # grok.name('view')
+    def update(self):
+        super(View,self).update()
+        context = aq_inner(self.context)
+        self._path = '/'.join(context.getPhysicalPath())
+        self.state = getMultiAdapter((context, self.request), name=u'plone_context_state')
+        self.tools = getMultiAdapter((context, self.request), name=u'plone_tools')
+        self.portal = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+        self._ct = self.tools.catalog()
+        self._mt = self.tools.membership()
+        self.member = self.portal.member()
+        roles_context = self.member.getRolesInContext(context)
+        if not self.show_border:
+            self.request['disable_border'] = True
+    
+    
+    @property
+    def can_submit(self):
+        ''' This user can submit a talk in here'''
+        context = self.context
+        return self._mt.checkPermission('apyb.papers: Add Talk',context)
+    
+    @property
+    def show_border(self):
+        ''' Is this user allowed to edit this content '''
+        return self.state.is_editable()
+    
+    def speaker_name(self,speaker_uids):
+        ''' Given a list os uids, we return a string with speakers names '''
+        ct = self._ct
+        results = ct.searchResults(portal_type='apyb.papers.speaker',UID=speaker_uids)
+        return ', '.join([b.Title for b in results])
+    
+    def tracks(self):
+        ''' Return a list of tracks in here '''
+        results = self._ct.searchResults(portal_type='apyb.papers.track', 
+                                         path=self._path,
+                                         sort_on='getObjPositionInParent')
+        return results
+    
+    def speakers(self):
+        ''' Return a list of speakers in here '''
+        results = self._ct.searchResults(portal_type='apyb.papers.speaker',
+                                         path=self._path,        
+                                         sort_on='sortable_title')
+        return results
+    
+    def talks(self):
+        ''' Return a list of talks in here '''
+        results = self._ct.searchResults(portal_type='apyb.papers.talk', 
+                                         path=self._path,
+                                         sort_on='sortable_title')
+        return results
+    
+    def my_talks(self):
+        ''' Return a list of my talks '''
+        results = self._ct.searchResults(portal_type='apyb.papers.talk', 
+                                         path=self._path,
+                                         Creator=self.member.getUserName(),
+                                         sort_on='sortable_title')
+        return results
+    
+    def my_profiles(self):
+        ''' Return a list of my speaker profiles '''
+        results = self._ct.searchResults(portal_type='apyb.papers.speaker', 
+                                         path=self._path,
+                                         Creator=self.member.getUserName(),
+                                         sort_on='sortable_title')
+        return results
+    
+    def last_talks(self):
+        ''' Return a list of the last 5 talks in here '''
+        results = self._ct.searchResults(portal_type='apyb.papers.talk', 
+                                         path=self._path,
+                                         sort_on='created',
+                                         sort_order='reverse',
+                                         sort_limit=5,)
+        return results[:5]
+    
+class Speakers(grok.View):
+    grok.context(IProgram)
+    grok.require('zope2.View')
+    grok.name('speakers')
+    
+    def update(self):
+        super(View,self).update()
+        context = aq_inner(self.context)
+        self._path = '/'.join(context.getPhysicalPath())
+        self.state = getMultiAdapter((context, self.request), name=u'plone_context_state')
+        self.tools = getMultiAdapter((context, self.request), name=u'plone_tools')
+        self.portal = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+        self._ct = self.tools.catalog()
+        self.member = self.portal.member()
+        roles_context = self.member.getRolesInContext(context)
+        if not show_border:
+            self.request['disable_border'] = True
+    
+    
+    def speakers(self):
+        ''' Return a list of speakers in here '''
+        results = self._ct.searchResults(portal_type='apyb.papers.speaker',
+                                         path=self._path,        
+                                         sort_on='sortable_title')
+        return results
+    
+    @property
+    def show_border(self):
+        ''' Is this user allowed to edit this content '''
+        return self.state.is_editable()
+    
+    
+
+  
