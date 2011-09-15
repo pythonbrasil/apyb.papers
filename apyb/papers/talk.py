89f63bac72d47a55bf50397306c40ab9b1799a30
Érico Andrei <erico@simplesconsultoria.com.br>
2011-09-14 23:46:07 -0300

    Add indexers for start and end PEP 8 patrol Imports reordering

--- apyb/papers/talk.py
+++ apyb/papers/talk.py
@@ -1,61 +1,58 @@
 # -*- coding:utf-8 -*
-
 import json
-from datetime import datetime
+from five import grok
 
+from DateTime import DateTime
+from Acquisition import aq_inner
 from Acquisition import aq_parent
 
-from five import grok
-from plone.directives import dexterity, form
-from z3c.relationfield.schema import RelationChoice, RelationList
-
-from Acquisition import aq_inner, aq_parent
-from zope.component import getMultiAdapter
-
-from Products.CMFCore.utils import getToolByName
-
-from zope.component import getUtility
 from zope.app.intid.interfaces import IIntIds
 
-from z3c.formwidget.query.interfaces import IQuerySource
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+
+from zope.interface import Interface
+from zope.interface import implements
+from zope.interface import  implementer
 
 from zope import schema
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zope.interface import Interface, implements
 from zope.schema.interfaces import IContextSourceBinder
-
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
 
-from collective.z3cform.datagridfield import DataGridFieldFactory, IDataGridField
-from collective.z3cform.datagridfield import DictRow
-
-from plone.formwidget.autocomplete.widget import AutocompleteMultiSelectionWidget
-
-from Products.CMFPlone.utils import normalizeString
-
-from z3c.form import group, field
-from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
-
-from apyb.papers import MessageFactory as _
-from zope.interface import  implementer
 import z3c.form.interfaces
 import z3c.form.widget
+from z3c.formwidget.query.interfaces import IQuerySource
+
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import normalizeString
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.indexer import indexer
+from plone.directives import dexterity, form
+
+from plone.formwidget.autocomplete.widget import \
+     AutocompleteMultiSelectionWidget
+
+from collective.z3cform.datagridfield import DictRow
+
+from apyb.papers import MessageFactory as _
+
 
 class SpeakerWidget(AutocompleteMultiSelectionWidget):
     ''' Override input template for AutocompleteMultiFieldWidget '''
-    
+
     input_template = ViewPageTemplateFile('talk_templates/input.pt')
-    
+
     def new_speaker_url(self):
         ''' Return an url to @@new-speaker '''
         context = aq_inner(self.context)
-        
-        while context.portal_type not in ['apyb.papers.program','Plone Site',]:
+        while context.portal_type not in ['apyb.papers.program', 'Plone Site']:
             context = aq_parent(context)
         if context.portal_type == 'apyb.papers.program':
             program_url = context.absolute_url()
             return '%s/@@new-speaker' % program_url
+
 
 @implementer(z3c.form.interfaces.IFieldWidget)
 def SpeakerFieldWidget(field, request):
@@ -68,34 +65,39 @@ class SpeakerSource(object):
 
     def __init__(self, context):
         self.context = aq_inner(context)
-    
+
     @property
     def speakers(self):
         return self.find_speakers()
-    
+
     @property
     def all_speakers(self):
         return self.find_speakers(filter=False)
-    
+
     def find_speakers(self, filter=True):
-        mt = getToolByName(self.context,'portal_membership')
-        ct = getToolByName(self.context,'portal_catalog')
+        mt = getToolByName(self.context, 'portal_membership')
+        ct = getToolByName(self.context, 'portal_catalog')
         member = mt.getAuthenticatedMember()
         rolesHere = member.getRolesInContext(self.context)
-        dictSearch = {'portal_type':'apyb.papers.speaker','sort_on':'sortable_title'}
-        if filter and not [r for r in rolesHere if r in ['Manager','Reviewer']]:
-            # Only list profiles created by this user
+        dictSearch = {'portal_type': 'apyb.papers.speaker',
+                      'sort_on': 'sortable_title'}
+        if filter and not [r for r in rolesHere
+                                   if r in ['Manager', 'Reviewer']]:
+        #Only list profiles created by this user
             dictSearch['Creator'] = member.getUserName()
         return ct.searchResults(**dictSearch)
-        
+
     @property
     def vocab(self):
-        return SimpleVocabulary([SimpleTerm(b.UID,b.UID,b.Title) for b in self.speakers if hasattr(b,'UID')])
-    
+        return SimpleVocabulary([SimpleTerm(b.UID, b.UID, b.Title)
+                                for b in self.speakers if hasattr(b, 'UID')])
+
     @property
     def unfiltered_vocab(self):
-        return SimpleVocabulary([SimpleTerm(b.UID,b.UID,b.Title) for b in self.all_speakers if hasattr(b,'UID')])
-    
+        return SimpleVocabulary([SimpleTerm(b.UID, b.UID, b.Title)
+                                 for b in self.all_speakers
+                                 if hasattr(b, 'UID')])
+
     def __contains__(self, term):
         return self.vocab.__contains__(term)
 
@@ -118,14 +120,15 @@ class SpeakerSource(object):
         except LookupError:
             term = self.unfiltered_vocab.getTermByToken(value)
         return term
-    
-    def normalizeString(self,value):
+
+    def normalizeString(self, value):
         context = self.context
-        return normalizeString(value,context).lower()
-    
+        return normalizeString(value, context).lower()
+
     def search(self, query_string):
         q = self.normalizeString(query_string)
-        return [self.getTerm(b.UID) for b in self.speakers if q in self.normalizeString(b.Title)]
+        return [self.getTerm(b.UID) for b in self.speakers
+                                    if q in self.normalizeString(b.Title)]
 
 
 class SpeakerSourceBinder(object):
@@ -137,26 +140,25 @@ class SpeakerSourceBinder(object):
 
 class ITalkReference(Interface):
     ''' A talk reference'''
-    
+
     ref_type = schema.Choice(
                     title=_(u'Reference type'),
                     description=_(u'Type of this reference'),
                     required=True,
                     vocabulary='apyb.papers.talk.referencetype',
     )
-    
+
     url = schema.TextLine(
        title=_(u'URL'),
        description=_(u'Please provide a link to the reference'),
        required=True,
     )
-    
+
     description = schema.TextLine(
        title=_(u'Description'),
        description=_(u'Please provide a brief description of this reference'),
        required=True,
     )
-    
 
 
 class ITalk(form.Schema):
@@ -168,30 +170,32 @@ class ITalk(form.Schema):
         title=_(u"uid"),
         required=False,
         )
-    #
+
     form.widget(speakers=SpeakerFieldWidget)
     speakers = schema.List(
         title=_(u'Speaker'),
-        description=_(u'Please fill in the name of the speaker. If no speaker profile was created for this name, click on Add new speaker'),
+        description=_(u'Please fill in the name of the speaker. If no speaker '
+                      u'profile was created for this name, click on '
+                      u'Add new speaker'),
         default=[],
         value_type=schema.Choice(title=_(u"Speaker"),
                                  source=SpeakerSourceBinder()),
         required=True,
         )
-    #
+
     title = schema.TextLine(
         title=_(u'Talk Title'),
         description=_(u'Inform a talk title'),
         required=True,
     )
-    #
+
     form.widget(text='plone.app.z3cform.wysiwyg.WysiwygFieldWidget')
     text = schema.Text(
         title=_(u"Talk details"),
         required=True,
         description=_(u"A description of this talk"),
     )
-    #
+
     dexterity.read_permission(talk_type='zope2.View')
     dexterity.write_permission(talk_type='apyb.papers.AllocateTalk')
     talk_type = schema.Choice(
@@ -200,48 +204,50 @@ class ITalk(form.Schema):
         description=_(u"Which type of talk best describes this one"),
         vocabulary='apyb.papers.talk.type',
     )
-    #
+
     language = schema.Choice(
         title=_(u"Language"),
         required=True,
         description=_(u"Speaker's language"),
         vocabulary='apyb.papers.languages',
     )
-    #
+
     track = schema.Choice(
         title=_(u"Track"),
         required=True,
         description=_(u"Which track this talk is"),
         vocabulary='apyb.papers.talk.track',
     )
-    # 
+
     level = schema.Choice(
         title=_(u"Level"),
         required=True,
         description=_(u"Level of this talk"),
         vocabulary='apyb.papers.talk.level',
     )
-    #
-    form.widget(references='collective.z3cform.datagridfield.DataGridFieldFactory')
+
+    form.widget(
+        references='collective.z3cform.datagridfield.DataGridFieldFactory')
     references = schema.List(
         title=_(u"References"),
         required=False,
         description=_(u"References for this talk"),
-        value_type=DictRow(title=_(u'Reference'), 
+        value_type=DictRow(title=_(u'Reference'),
                                 schema=ITalkReference),
     )
-    #
+
     iul = schema.Bool(
-        title=_(u"Do you allow your image to be used in post-conference videos?"),
+        title=_(u'Do you allow your image to be used in '
+                u'post-conference videos?'),
         required=False,
         default=True,
     )
-    #
+
     form.fieldset('allocation',
             label=_(u"Talk Allocation"),
-            fields=['points','startDate','endDate','location']
+            fields=['points', 'startDate', 'endDate', 'location'],
     )
-    
+
     dexterity.read_permission(points='zope2.View')
     dexterity.write_permission(location='apyb.papers.AllocateTalk')
     points = schema.Float(
@@ -249,18 +255,19 @@ class ITalk(form.Schema):
         description = _(u""),
         required = False,
     )
-    
+
     dexterity.read_permission(votes='zope2.View')
     dexterity.write_permission(location='apyb.papers.AllocateTalk')
     form.omitted('votes')
     votes = schema.Dict(
         title =_(u"Votes"),
-        description = _(u"Votes for this talk. Position is relative to track."),
+        description = _(u'Votes for this talk. Position is'
+                        u'relative to track.'),
         required = False,
     )
-    
+
     dexterity.read_permission(location='zope2.View')
-    dexterity.write_permission(location='apyb.papers.AllocateTalk')    
+    dexterity.write_permission(location='apyb.papers.AllocateTalk')
     location = schema.Choice(
         title=_(u"Location"),
         required=False,
@@ -275,7 +282,7 @@ class ITalk(form.Schema):
         required=False,
         description=_(u"Talk start date"),
     )
-    #
+
     dexterity.read_permission(endDate='zope2.View')
     dexterity.write_permission(endDate='apyb.papers.AllocateTalk')
 #    form.widget(endDate='collective.z3cform.datetimewidget.DatetimeWidget')
@@ -284,12 +291,12 @@ class ITalk(form.Schema):
         required=False,
         description=_(u"Talk end date"),
     )
-    #
+
     form.fieldset('material',
             label=_(u"Talk materials"),
-            fields=['presentation','video','files']
+            fields=['presentation', 'video', 'files'],
     )
-    
+
     dexterity.read_permission(presentation='zope2.View')
     dexterity.write_permission(presentation='apyb.papers.AllocateTalk')
     presentation = schema.TextLine(
@@ -297,7 +304,7 @@ class ITalk(form.Schema):
         required=False,
         description=_(u"Link to the presentation file"),
     )
-    #
+
     dexterity.read_permission(video='zope2.View')
     dexterity.write_permission(video='apyb.papers.AllocateTalk')
     video = schema.TextLine(
@@ -305,7 +312,7 @@ class ITalk(form.Schema):
         required=False,
         description=_(u"Link to the presentation video"),
     )
-    #
+
     dexterity.read_permission(files='zope2.View')
     dexterity.write_permission(files='apyb.papers.AllocateTalk')
     files = schema.TextLine(
@@ -313,90 +320,110 @@ class ITalk(form.Schema):
         required=False,
         description=_(u"Link to the presentation file downloads"),
     )
-    #
+
 
 class Talk(dexterity.Item):
     grok.implements(ITalk)
-    
+
     def Title(self):
         return self.title
-    
+
     def UID(self):
         return self.uid
-    
+
     @property
     def uid(self):
         intids = getUtility(IIntIds)
         return intids.getId(self)
 
 
+@indexer(ITalk)
+def startIndexer(obj):
+    if obj.startDate is None:
+        return None
+    return DateTime(obj.startDate.isoformat())
+grok.global_adapter(startIndexer, name="start")
+
+
+@indexer(ITalk)
+def endIndexer(obj):
+    if obj.endDate is None:
+        return None
+    return DateTime(obj.endDate.isoformat())
+grok.global_adapter(endIndexer, name="end")
+
+
 class View(dexterity.DisplayForm):
     grok.context(ITalk)
     grok.require('zope2.View')
-    
+
     def update(self):
-        super(View,self).update()
+        super(View, self).update()
         context = aq_inner(self.context)
         self.context = context
         self._path = '/'.join(context.getPhysicalPath())
-        self.state = getMultiAdapter((context, self.request), name=u'plone_context_state')
-        self.tools = getMultiAdapter((context, self.request), name=u'plone_tools')
-        self.portal = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+        self.state = getMultiAdapter((context, self.request),
+                                     name=u'plone_context_state')
+        self.tools = getMultiAdapter((context, self.request),
+                                     name=u'plone_tools')
+        self.portal = getMultiAdapter((context, self.request),
+                                     name=u'plone_portal_state')
         self._ct = self.tools.catalog()
         self._mt = self.tools.membership()
         self._wt = self.tools.workflow()
         self.member = self.portal.member()
-        roles_context = self.member.getRolesInContext(context)
+        self.roles_context = self.member.getRolesInContext(context)
         if not self.show_border:
-            self.request['disable_border'] = True    
+            self.request['disable_border'] = True
+
     @property
     def show_border(self):
         ''' Is this user allowed to edit this content '''
         return self.state.is_editable()
-    
+
     @property
     def show_references(self):
         ''' If this talk has references, show it '''
         context = self.context
         references = context.references
         return references
-    
+
     def speaker_info(self):
         ''' return information about speakers to this talk '''
         speakers = self.context.speakers
         ct = self._ct
-        results = ct.searchResults(portal_type='apyb.papers.speaker',UID=speakers)
-        
+        results = ct.searchResults(portal_type='apyb.papers.speaker',
+                                   UID=speakers)
         return results
-    
+
+
 class JSONView(View):
     grok.context(ITalk)
     grok.require('zope2.View')
     grok.name('json')
-    
+
     template = None
-    
+
     def speakers(self):
         ''' Return a list of speakers in here '''
-        brains = super(JSONView,self).speaker_info()
+        brains = super(JSONView, self).speaker_info()
         speakers = []
         for brain in brains:
-            speaker = {'name':brain.Title,
-                       'organization':brain.organization,
-                       'bio':brain.Description,
-                       'country':brain.country,
-                       'state':brain.state,
-                       'city':brain.city,
-                       'language':brain.language,
-                       'url':brain.getURL(),
-                       'json_url':'%s/json' % brain.getURL(),
+            speaker = {'name': brain.Title,
+                       'organization': brain.organization,
+                       'bio': brain.Description,
+                       'country': brain.country,
+                       'state': brain.state,
+                       'city': brain.city,
+                       'language': brain.language,
+                       'url': brain.getURL(),
+                       'json_url': '%s/json' % brain.getURL(),
                        }
             speakers.append(speaker)
         return speakers
-    
+
     def render(self):
-        request = self.request
-        data = {'speakers':self.speakers()}
+        data = {'speakers': self.speakers()}
         data['url'] = self.context.absolute_url()
         data['title'] = self.context.title
         data['summary'] = self.context.text
@@ -407,7 +434,7 @@ class JSONView(View):
         data['text'] = self.context.talk_type
         data['level'] = self.context.level
         data['points'] = self.context.points or 0.0
-        data['state'] = self._wt.getInfoFor(self.context,'review_state')
-                                        
-        self.request.response.setHeader('Content-Type', 'application/json;charset=utf-8')
-        return json.dumps(data,encoding='utf-8',ensure_ascii=False)
+        data['state'] = self._wt.getInfoFor(self.context, 'review_state')
+        self.request.response.setHeader('Content-Type',
+                                        'application/json;charset=utf-8')
+        return json.dumps(data, encoding='utf-8', ensure_ascii=False)
